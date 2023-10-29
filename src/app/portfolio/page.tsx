6a2fc853d3a12f0304048020857b6f0d1e8fe5f2
Alexey Sevastynov <alexseva94@gmail.com>
2023-10-29 18:49:15 +0200

    added content page portfolio

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <section className={styles.portfolio}>
+      <h2 className={styles.selectTitle}>Choose a galary</h2>
+      <div className={styles.items}>
+        <Link href="portfolio/ilustrations" className={styles.item}>
+          <span className={styles.title}>Ilustrations</span>
+        </Link>
+        <Link href="portfolio/website" className={styles.item}>
+          <span className={styles.title}>Website</span>
+        </Link>
+        <Link href="portfolio/aplication" className={styles.item}>
+          <span className={styles.title}>Aplication</span>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
