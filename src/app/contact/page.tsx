dbd0c73376201f82d7ce91669c46235db0482bff
Alexey Sevastynov <alexseva94@gmail.com>
2023-10-29 18:33:49 +0200

    added content page contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/button";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className={styles.contact}>
+      <h2 className={styles.title}>Let`s Keep in touch</h2>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt="contact-image"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            placeholder="message"
+            cols={30}
+            rows={10}
+            className={styles.textArea}
+          />
+          <Button type="button">Send</Button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
